c9c0ed9a9a87a8baffedc7c8f08db19969526c5e
Porfirio <porfirioribeiro@gmail.com>
2014-10-16 21:07:50 +0100

    Added a Log class to log only when compiled in debug mode

---
+++ src/hxtag/tools/Log.hx
@@ -0,0 +1,42 @@
+package hxtag.tools;
+
+import haxe.macro.Expr;
+import haxe.macro.Context;
+
+/**
+ * ...
+ * @author Porfírio
+ */
+class Log
+{
+	#if macro 
+	static function _ifd(e:Expr) {
+		if (Context.defined("debug"))
+			return e;
+		return macro { };		
+	}
+	#end
+	macro public static function v(args:Array<Expr>) 
+		return _ifd(macro hxtag.Dom.console.log($a{args}));
+	
+	macro public static function e(args:Array<Expr>) 
+		return _ifd(macro hxtag.Dom.console.error($a{args}));
+	
+	macro public static function w(args:Array<Expr>) 
+		return _ifd(macro hxtag.Dom.console.warn($a{args}));
+	
+	macro public static function i(args:Array<Expr>) 
+		return _ifd(macro hxtag.Dom.console.info($a{args}));
+	
+	macro public static function v_if(expr:Expr,args:Array<Expr>) 
+		return _ifd(macro if ($expr) hxtag.Dom.console.log($a { args } ));	
+		
+	macro public static function e_if(expr:Expr,args:Array<Expr>) 
+		return _ifd(macro if ($expr) hxtag.Dom.console.error($a { args } ));	
+		
+	macro public static function w_if(expr:Expr,args:Array<Expr>) 
+		return _ifd(macro if ($expr) hxtag.Dom.console.warn($a { args } ));	
+		
+	macro public static function i_if(expr:Expr,args:Array<Expr>) 
+		return _ifd(macro if ($expr) hxtag.Dom.console.info($a{args}));
+}
